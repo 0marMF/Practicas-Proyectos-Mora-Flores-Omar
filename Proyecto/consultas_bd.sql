90d751e2b03c1e2ad52759c174eb2bb236e864fb
0marMF <omar13mor@gmail.com>
2025-03-30 17:10:15 -0600

    Create consultas_bd.sql
    Creación de consultas y consulta general

---
+++ Proyecto/consultas_bd.sql
@@ -0,0 +1,9 @@
+-- Seleccionar la base de datos
+USE gimnasio_BD;
+GO
+
+-- Consultar tablas  base
+select *  from admin; 
+select *  from gerente; 
+select *  from miembros; 
+select *  from acceso;
